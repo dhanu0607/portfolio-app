83b411b34387c3a776419406b51699f198e30907
dhanu0607 <dhanupoojary0607@gmail.com>
2025-08-28 15:54:15 +0530

    update portfolio

--- client/src/components/hero.js
+++ client/src/components/hero.js
@@ -2,19 +2,19 @@ export default function Hero() {
   return (
     <section
       id="home"
-      className="min-h-screen flex items-center justify-center  from-cyan-950  text-white px-6"
+      className="min-h-screen flex items-center justify-center  from-cyan-950  text-white px:4 sm:px-6"
     >
       {/* Outer Box with Gradient + Glow */}
-      <div className="w-full max-w-6xl h-[600px] rounded-lg border-2 border-cyan-400 bg-gradient-to-r from-cyan-500 to-gray-900 relative">
+      <div className="w-full max-w-6xl h-[600px] rounded-lg border-2 border-cyan-400 bg-gradient-to-r from-cyan-500 to-gray-900 relative p-6 sm:p-10">
         
        
 
         {/* Main Content Split */}
-        <div className="flex h-[calc(100%-80px)]">
+        <div className="flex flex-col md:flex-row items-center h-full">
           
           {/* Left Side */}
-          <div className="w-1/2 flex flex-col justify-center px-12">
-            <h1 className="text-5xl font-extrabold leading-tight mb-6">
+          <div className="w-full md:w-1/2 flex flex-col justify-center text-center md:text-left md-8 md:md-0 px-2 sm:px-6">
+            <h1 className="text-3xl sm:text-4xl md:text-5xl font-extrabold leading-tight mb-6">
               HELLO, <br />
               I AM A <br />
               <span className="bg-cyan-700 text-white px-2">WEB DEVELOPER</span>
@@ -29,11 +29,11 @@ export default function Hero() {
           </div>
 
           {/* Right Side */}
-          <div className="w-1/2 flex items-center justify-center">
+          <div className="w-fullmd:w-1/2 flex items-center justify-center">
             <img
               src="/profile.jpg"
               alt="profile"
-              className="w-80 h-auto object-cover rounded-lg border-2 border-cyan-400 shadow-[0_0_30px_rgba(34,211,238,0.8)]"
+              className="w-40 sm:w-56 md:w-80 h-auto object-cover rounded-lg border-2 border-cyan-400 shadow-[0_0_30px_rgba(34,211,238,0.8)]"
             />
           </div>
         </div>
